5800c0f11f808e7af10d23c5f6d3c82fe710b7b9
Svend Tofte <stofte@gmail.com>
2014-08-25 23:08:21 +0200

    foo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var faye = require('faye');
 var simpleHttp = require('./simple-http');
 
 var prefix = '/www';
-var port = 80; //process.env.PORT || 3000;
+var port = 81; //process.env.PORT || 3000;
 
 var id = 0;
 var textData = [
@@ -43,7 +43,7 @@ simpleHttp.start(port); // launches the port 80 server
 var wsHttpServer = http.createServer();
 var fayeServer = new faye.NodeAdapter({mount: '/ws'});
 fayeServer.attach(wsHttpServer);
-wsHttpServer.listen(8080);
+wsHttpServer.listen(8888);
 
 var wsClient = new faye.Client('http://127.0.0.1:8080/ws');
 
