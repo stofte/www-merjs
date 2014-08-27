f707723479da08e8b2a6996c3d28fa0b295bf48c
Svend Tofte <stofte@gmail.com>
2014-08-28 01:11:06 +0200

    asdqq

--- app.js
+++ app.js
@@ -1,31 +1,17 @@
 var http = require('http');
+var simpleHttp = require('./simple-http');
 var WebSocketServer = require('ws').Server;
-var wss = new WebSocketServer({port: 8081});
+var textData = require('./data').data;
 
-var id = 0;
-var textData = [
-  {x: 460, y: 26, c: 'm',   x2: 0, y2: 0, id: id++ },
-  {x: 460, y: 122, c: 'e',  x2: 0, y2: 0, id: id++ },
-  {x: 460, y: 199, c: 'd',  x2: 0, y2: 0, id: id++ },
-
-  {x: 460, y: 344, c: 'm',  x2: 0, y2: 0, id: id++ },
-  {x: 460, y: 441, c: 'e',  x2: 0, y2: 0, id: id++ },
-  {x: 460, y: 505, c: 'r',  x2: 0, y2: 0, id: id++ },
-  {x: 460, y: 570, c: 'e',  x2: 0, y2: 0, id: id++ },
-
-  {x: 460, y: 685, c: 'J',  x2: 0, y2: 0, id: id++ },
-  {x: 460, y: 775, c: 'a',  x2: 0, y2: 0, id: id++ },
-  {x: 460, y: 855, c: 'v',  x2: 0, y2: 0, id: id++ },
-  {x: 460, y: 934, c: 'a',  x2: 0, y2: 0, id: id++ },
-  {x: 460, y: 1027, c: 'S', x2: 0, y2: 0, id: id++ },
-  {x: 460, y: 1108, c: 'c', x2: 0, y2: 0, id: id++ },
-  {x: 460, y: 1172, c: 'r', x2: 0, y2: 0, id: id++ },
-  {x: 460, y: 1227, c: 'i', x2: 0, y2: 0, id: id++ },
-  {x: 460, y: 1290, c: 'p', x2: 0, y2: 0, id: id++ },
-  {x: 460, y: 1352, c: 't', x2: 0, y2: 0, id: id++ },
-  {x: 460, y: 1396, c: '!', x2: 0, y2: 0, id: id++ }
-];
-console.log('process.env.AWS_EC2', process.env.AWS_EC2);
+// seems hardwired in eb container, messing with nginx doesnt help either
+var port = 8081;
+// when running locally, we start a basic http server in place of nginx.
+if (!process.env.AWS_EC2) {
+    simpleHttp.start(port);
+}
+// simple-http doesn't handle websockets, so we switch port, also in client.
+var wsport = process.env.AWS_ECS ? port : port + 1;
+var wss = new WebSocketServer({port: wsport});
 
 wss.on('connection', function(websocket) {
   console.log('websocket request on:', websocket.upgradeReq.url);
