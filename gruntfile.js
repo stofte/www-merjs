f79f57f35890949798e8ed409d20b50073d3a9a5
Svend Tofte <stofte@gmail.com>
2014-08-31 13:24:05 +0200

    grunt setup
    license preservation + minify

--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,5 @@
+var saveLicense = require('uglify-save-license');
+
 module.exports = function(grunt) {
 	'use strict';
 
@@ -11,9 +13,26 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+		uglify: {
+			options: {
+				preserveComments: 'some'
+			},
+			production: {
+				files: {
+					'www/min.js': [
+						'src/StackBlur.js',
+						'src/font-detect.js',
+						'src/guid.js',
+						'src/client.js',
+						'src/js.js'
+					]
+				}
+			}
+		}		
 
 	};
 
 	grunt.initConfig(conf);
 	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 };
